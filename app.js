035c3bee677f7d82edc61d3c68e6006db65024d5
Gabriel Acosta <gabriacos@gmail.coma>
2023-06-21 14:39:53 -0300

    Se completo el codigo faltante para la implementación de los middlewares de cors y el express.json, se realizó la instalación con npm de dotenv, ejs y sequelice para la implementación del proyecto, se agregó tambien el require para ejs y dotenv para la utilización del archivo para las variables de entorno..

--- app.js
+++ app.js
@@ -4,12 +4,20 @@ const express = require('express');
 
 const path = require('path');
 
+require('dotenv').config();
+require('ejs')
 const app = express();
 
 // Middlewares
 // TODO: Implementar middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+
+const port = process.env.PORT || 3000;
 
 // Routes
 app.use('/api', require('./routes/reserva.routes'));
@@ -17,4 +25,4 @@ app.use('/api', require('./routes/reserva.routes'));
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
 
 // Starting the server
-app.listen(45635, () => console.log('Server on port xxxx'));
+app.listen(port, () => console.log(`Server on port http://localhost:${port}/api/`));
